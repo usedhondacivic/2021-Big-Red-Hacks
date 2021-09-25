5f05836323b3750d96d16715b77233daa6528f07
usedhondacivic <micacer48@gmail.com>
2021-09-25 14:42:48 -0400

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className = "header">
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class Pane extends React.Component {
+  render() {
+    let className = "pane" + this.props.size;
+    return (
+      <div className = {className + " pane"}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class SubContainer extends React.Component {
+  render() {
+    let className = "subContainer" + this.props.size;
+    return <div className = {className + " subContainer"}>
+      {this.props.children}
+    </div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        <h1 className = "title">The Space Times Continuum</h1>
+      </Header>
+      <div className = "contentContainer">
+        <SubContainer size="2x2">
+          <Pane size = "2x1"></Pane>
+          <Pane size = "2x1"></Pane>
+        </SubContainer>
+        <SubContainer size="2x2">
+          <Pane size = "2x1"></Pane>
+          <Pane size = "1x1"></Pane>
+          <Pane size = "1x1"></Pane>
+        </SubContainer>
+        <Pane size = "2x1"></Pane>
+        <Pane size = "2x2"></Pane>
+        <Pane size = "2x2"></Pane>
+      </div>
     </div>
   );
 }
